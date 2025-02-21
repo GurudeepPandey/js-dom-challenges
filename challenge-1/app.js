24861a7ea2bac1eb8e23cd9551c0bb57c6d4740f
Gurudeep Pandey <pandeygurudeep@gmail.com>
2025-02-21 19:48:30 +0530

    challenge1 complete

---
+++ challenge-1/app.js
@@ -0,0 +1,29 @@
+const button = document.querySelector("#toggleButton")
+const statusbtn = document.querySelector("#status")
+const bulb = document.querySelector("#bulb")
+const btnclick = 0
+
+// button.addEventListener("click", () => {
+//     document.body.classList.toggle("dark-mode")
+//     bulb.classList.toggle("bulbon")
+//     if(btnclick === 0) {
+//         btnclick = 1
+//         button.innerText = "Turn Off"
+//         statusbtn.innerText = "Status: On"
+//     }
+//     else {
+//         btnclick = 0
+//         button.innerText = "Turn On"
+//         statusbtn.innerText = "Status: Off"
+//     }
+// })
+
+
+const body = document.querySelector("body");
+button.addEventListener("click", () => {
+    const isDark = body.classList.toggle("dark-mode");
+    bulb.classList.toggle("bulbon");
+
+    button.innerText = isDark ? "Turn On" : "Turn Off";
+    statusbtn.innerText = `Status: ${isDark ? "Off" : "On"}`;
+});
